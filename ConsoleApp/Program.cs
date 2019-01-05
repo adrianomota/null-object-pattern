3ba2f8ec4d9a67e3085a7794d4fdff16a95fbf27
Adriano Mota <adrianowsh@Hotmail.com>
2019-01-05 15:37:46 -0200

    commit inicial

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            PlayerCharacter sarah = new PlayerCharacter(new DiamondSkinDefense());
+            sarah.SetName("Sarah");
+
+            PlayerCharacter amrit = new PlayerCharacter(SpecialDefense.Null);
+            amrit.SetName("Amrit");
+
+            PlayerCharacter gentry = new PlayerCharacter(SpecialDefense.Null);
+            gentry.SetName("Gentry");
+
+            sarah.Hit(10);
+            amrit.Hit(10);
+            gentry.Hit(10);
+
+            Console.ReadLine();
+
+            await Task.FromResult(0);
+        }
+    }
+}
